7b1579b7de146bf6fd073d476b61e9763d31f9ed
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-05-13 10:38:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,10 @@ function green_light () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
 }
-function countdown (sound_: boolean, walk: boolean) {
+input.onButtonPressed(Button.A, function () {
+    Traffic_countdowmn(false, true)
+})
+function Traffic_countdowmn (sound_: boolean, walk: boolean) {
     green_light()
     if (walk) {
         basic.showIcon(IconNames.StickFigure)
@@ -28,14 +31,16 @@ function countdown (sound_: boolean, walk: boolean) {
     }
     red_light()
 }
-input.onButtonPressed(Button.A, function () {
-    countdown(false, true)
-})
 input.onButtonPressed(Button.AB, function () {
-    countdown(false, false)
+    Traffic_countdowmn(false, false)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "") {
+    	
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    countdown(true, true)
+    Traffic_countdowmn(true, true)
 })
 function red_light () {
     range = strip.range(0, 3)
@@ -53,7 +58,7 @@ let distance = 0
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Yes)
-strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 strip.setBrightness(255)
 basic.showLeds(`
     . . # # .
@@ -63,14 +68,22 @@ basic.showLeds(`
     . # # # .
     `)
 red_light()
+radio.setGroup(58)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    if (distance < 0) {
-    	
+    if (distance == 5) {
+        Traffic_countdowmn(false, false)
+        basic.showLeds(`
+            . . # # .
+            . . # # #
+            . # # # #
+            . # # # #
+            . # # # .
+            `)
     }
 })
