18104dd01c2fadc3c390c29dca80e671c3d1eb9d
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-05-17 10:39:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        green_light()
+    } else {
+        Traffic_countdowmn(false, false)
+    }
+})
 function green_light () {
     range = strip.range(0, 3)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -8,9 +15,10 @@ input.onButtonPressed(Button.A, function () {
     Traffic_countdowmn(false, true)
 })
 function Traffic_countdowmn (sound_: boolean, walk: boolean) {
+    basic.pause(3000)
     green_light()
+    basic.showIcon(IconNames.StickFigure)
     if (walk) {
-        basic.showIcon(IconNames.StickFigure)
         for (let index2 = 0; index2 <= 20; index2++) {
             basic.showNumber(20 - index2)
             if (sound_) {
@@ -18,6 +26,14 @@ function Traffic_countdowmn (sound_: boolean, walk: boolean) {
             }
         }
     }
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # .
+        . . # # #
+        . # # # #
+        . # # # #
+        . # # # .
+        `)
     yellow_light()
     basic.pause(3700)
     if (walk) {
@@ -34,11 +50,6 @@ function Traffic_countdowmn (sound_: boolean, walk: boolean) {
 input.onButtonPressed(Button.AB, function () {
     Traffic_countdowmn(false, false)
 })
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "") {
-    	
-    }
-})
 input.onButtonPressed(Button.B, function () {
     Traffic_countdowmn(true, true)
 })
@@ -54,10 +65,8 @@ function yellow_light () {
     range = strip.range(1, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Yellow))
 }
-let distance = 0
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
-basic.showIcon(IconNames.Yes)
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.setBrightness(255)
 basic.showLeds(`
@@ -67,23 +76,26 @@ basic.showLeds(`
     . # # # #
     . # # # .
     `)
+let count = 0
 red_light()
+let distance = 0
 radio.setGroup(58)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P16, PulseValue.High) / 58
-    if (distance < 5) {
-        Traffic_countdowmn(false, false)
-        basic.showLeds(`
-            . . # # .
-            . . # # #
-            . # # # #
-            . # # # #
-            . # # # .
-            `)
+    for (let index = 0; index < 10; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, pins.pulseIn(DigitalPin.P8, PulseValue.High) / 58)
+        if (distance == 5) {
+            Traffic_countdowmn(false, false)
+            basic.showLeds(`
+                . . # # .
+                . . # # #
+                . # # # #
+                . # # # #
+                . # # # .
+                `)
+        }
     }
 })
