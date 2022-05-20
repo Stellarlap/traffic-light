826cd43ccd568629163d133eaf4a6447f6f1afc9
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-05-19 21:29:34 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,55 +1,41 @@
-def Green_Light():
+def on_received_number(receivedNumber):
+    if receivedNumber == 0:
+        green_light()
+    else:
+        Traffic_Light_cycle(False, False)
+radio.on_received_number(on_received_number)
+
+def green_light():
     global range2
-    range2 = strip.range(0, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(1, 1)
+    range2 = strip.range(0, 3)
     range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
     range2 = strip.range(2, 1)
     range2.show_color(neopixel.colors(NeoPixelColors.GREEN))
 
 def on_button_pressed_a():
-    basic.show_icon(IconNames.STICK_FIGURE)
-    while True:
-        for index in range(21):
-            Green_Light()
-            basic.show_number(20 - index)
-        OFF()
-        yellow()
-        basic.pause(3700)
-        OFF2()
-        basic.show_leds("""
-            . . # # .
-                        . . # # #
-                        . # # # #
-                        . # # # #
-                        . # # # .
-        """)
-        Red()
-        basic.pause(20000)
-        off3()
+    Traffic_Light_cycle(False, True)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def OFF2():
-    global range2
-    range2 = strip.range(0, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(1, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(2, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-
-def on_button_pressed_b():
+def Traffic_Light_cycle(sound_: bool, walk: bool):
+    basic.pause(3000)
+    green_light()
     basic.show_icon(IconNames.STICK_FIGURE)
-    while True:
+    if walk:
         for index2 in range(21):
-            Green_Light()
             basic.show_number(20 - index2)
-            music.play_tone(440, music.beat(BeatFraction.WHOLE))
-        music.play_tone(220, music.beat(BeatFraction.QUARTER))
-        OFF()
-        yellow()
-        basic.pause(3700)
-        OFF2()
+            if sound_:
+                music.play_tone(440, music.beat(BeatFraction.WHOLE))
+    basic.pause(1000)
+    basic.show_leds("""
+        . . # # .
+                . . # # #
+                . # # # #
+                . # # # #
+                . # # # .
+    """)
+    yellow_light()
+    basic.pause(3700)
+    if walk:
         basic.show_leds("""
             . . # # .
                         . . # # #
@@ -57,46 +43,30 @@ def on_button_pressed_b():
                         . # # # #
                         . # # # .
         """)
-        Red()
-        basic.pause(20000)
-        off3()
+    red_light()
+
+def on_button_pressed_ab():
+    Traffic_Light_cycle(False, False)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    Traffic_Light_cycle(True, True)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def off3():
+def red_light():
     global range2
+    range2 = strip.range(0, 3)
+    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
     range2 = strip.range(0, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(1, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(2, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-def yellow():
+    range2.show_color(neopixel.colors(NeoPixelColors.RED))
+def yellow_light():
     global range2
-    range2 = strip.range(0, 1)
+    range2 = strip.range(0, 3)
     range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
     range2 = strip.range(1, 1)
     range2.show_color(neopixel.colors(NeoPixelColors.YELLOW))
-    range2 = strip.range(2, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-def OFF():
-    global range2
-    range2 = strip.range(0, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(1, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(2, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-def Red():
-    global range2
-    range2 = strip.range(0, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.RED))
-    range2 = strip.range(1, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    range2 = strip.range(2, 1)
-    range2.show_color(neopixel.colors(NeoPixelColors.BLACK))
 range2: neopixel.Strip = None
 strip: neopixel.Strip = None
-basic.show_icon(IconNames.YES)
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.set_brightness(255)
 basic.show_leds("""
@@ -106,9 +76,30 @@ basic.show_leds("""
         . # # # #
         . # # # .
 """)
-Red()
+count = 0
+red_light()
+distance = 0
+radio.set_group(58)
 
 def on_forever():
-    pins.digital_write_pin(DigitalPin.P0, 0)
-    control.wait_micros(5)
+    global distance, count
+    for index in range(10):
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        control.wait_micros(2)
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        control.wait_micros(10)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        distance = pins.pulse_in(DigitalPin.P2, PulseValue.HIGH) / 58
+        if distance <= 5:
+            count += 1
+            if count <= 10:
+                Traffic_Light_cycle(False, False)
+                count = 0
+            basic.show_leds("""
+                . . # # .
+                                . . # # #
+                                . # # # #
+                                . # # # #
+                                . # # # .
+            """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        green_light()
-    } else {
-        Traffic_Light_cycle(false, false)
-    }
-})
 function green_light () {
     range = strip.range(0, 3)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -15,11 +8,18 @@ input.onButtonPressed(Button.A, function () {
     Traffic_Light_cycle(false, true)
 })
 function Traffic_Light_cycle (sound_: boolean, walk: boolean) {
+    abort_cycle = false
     basic.pause(3000)
+    if (abort_cycle) {
+        return
+    }
     green_light()
-    basic.showIcon(IconNames.StickFigure)
     if (walk) {
+        basic.showIcon(IconNames.StickFigure)
         for (let index2 = 0; index2 <= 20; index2++) {
+            if (abort_cycle) {
+                return
+            }
             basic.showNumber(20 - index2)
             if (sound_) {
                 music.playTone(440, music.beat(BeatFraction.Whole))
@@ -27,15 +27,14 @@ function Traffic_Light_cycle (sound_: boolean, walk: boolean) {
         }
     }
     basic.pause(1000)
-    basic.showLeds(`
-        . . # # .
-        . . # # #
-        . # # # #
-        . # # # #
-        . # # # .
-        `)
+    if (abort_cycle) {
+        return
+    }
     yellow_light()
     basic.pause(3700)
+    if (abort_cycle) {
+        return
+    }
     if (walk) {
         basic.showLeds(`
             . . # # .
@@ -46,10 +45,14 @@ function Traffic_Light_cycle (sound_: boolean, walk: boolean) {
             `)
     }
     red_light()
+    basic.showLeds(`
+        . . # # .
+        . . # # #
+        . # # # #
+        . # # # #
+        . # # # .
+        `)
 }
-input.onButtonPressed(Button.AB, function () {
-    Traffic_Light_cycle(false, false)
-})
 input.onButtonPressed(Button.B, function () {
     Traffic_Light_cycle(true, true)
 })
@@ -59,14 +62,30 @@ function red_light () {
     range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Red))
 }
+radio.onReceivedValue(function (name, value) {
+    if (name == me) {
+        if (value == 1) {
+            abort_cycle = true
+            green_light()
+        } else if (value == 2) {
+            abort_cycle = true
+            red_light()
+        } else if (value == 3) {
+            Traffic_Light_cycle(false, false)
+        }
+    }
+})
 function yellow_light () {
     range = strip.range(0, 3)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(1, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Yellow))
 }
+let abort_cycle = false
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
+let me = ""
+me = "Dynes"
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.setBrightness(255)
 basic.showLeds(`
@@ -76,31 +95,25 @@ basic.showLeds(`
     . # # # #
     . # # # .
     `)
-let count = 0
 red_light()
-let distance = 0
+let distance = 6
+let count = 0
 radio.setGroup(58)
 basic.forever(function () {
-    for (let index = 0; index < 10; index++) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P8, 0)
         control.waitMicros(2)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
         control.waitMicros(10)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        distance = pins.pulseIn(DigitalPin.P13, PulseValue.High) / 58
         if (distance <= 5) {
             count += 1
+        }
+        if (count == 4) {
             Traffic_Light_cycle(false, false)
-            basic.showLeds(`
-                . . # # .
-                . . # # #
-                . # # # #
-                . # # # #
-                . # # # .
-                `)
+            count = 0
         }
     }
-    if (count == 10) {
-        Traffic_Light_cycle(false, false)
-    }
+    count = 0
 })
